53e9f1056ae507f45751f389ba4f67ad559337e8
Jiaqi-ZHOU <jiaqi.zhou@uclouvain.be>
2025-11-14 12:00:33 +0100

    The 1st version web

---
+++ compounds.js
@@ -0,0 +1,6 @@
+// compounds.js
+window.compounds = [
+    "CH4",
+    "CH4O",
+    "C2H2",
+];
---
+++ knownFiles.js
@@ -0,0 +1,10 @@
+// knownFiles.js
+window.getKnownFilesList = async function() {
+    return [
+        'C2H2_afa9aae6.xyz',
+        'C2H6_72334156.xyz',
+        'CH4O_dab9dad2.xyz',
+        'C2H2_ca2cc2cc.xyz',
+        'CH4_7ba71c46.xyz',
+    ];
+};
